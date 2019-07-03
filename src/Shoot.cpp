177ccf0182676cc957fe1f99e856b23aa2146f76
unknown <ahmedasmndr2@gmail.com>
2019-07-03 21:50:30 +0200

    cleaning

--- src/Shoot.cpp
+++ src/Shoot.cpp
@@ -5,9 +5,9 @@
 debug debugger_shoot;
 Servo shhot_motor;
 float delay_time_shoot;
-int angel=22;
-int motor_delay=100;
-int end_angel=0;
+int angel = 22;
+int motor_delay = 100;
+int end_angel = 0;
 
 void Shoot::motorIntil()
 {
@@ -16,82 +16,78 @@ void Shoot::motorIntil()
 }
 void Shoot::shootManuel_dropall()
 {
-  for (int i=0;i<15;i++)
+  for (int i = 0; i < 15; i++)
   {
-          shhot_motor.write(angel);
-          delay(motor_delay);
-          shhot_motor.write(end_angel); 
-          delay(500);
+    shhot_motor.write(angel);
+    delay(motor_delay);
+    shhot_motor.write(end_angel);
+    delay(500);
   }
-          
 }
 void Shoot::shootManuel()
 {
-  
-          shhot_motor.write(angel);
-          delay(motor_delay);
-          shhot_motor.write(end_angel); 
-          delay(500);
-          
+
+  shhot_motor.write(angel);
+  delay(motor_delay);
+  shhot_motor.write(end_angel);
+  delay(500);
 }
 
-void Shoot::fireBall(uint16_t delta_hoder,float total_time,uint8_t section, float time_resto , uint16_t window,uint16_t target_time)
+void Shoot::fireBall(uint16_t delta_hoder, float total_time, uint8_t section, float time_resto, uint16_t window, uint16_t target_time)
 {
-        //  if (time_resto < target_time+delta_hoder && time_resto >target_time)
-        // {
-        //   delay_time_shoot = time_resto - target_time-10;
-        //   if (delay_time_shoot<15000 )
-        //   {
-        //   delay(delay_time_shoot);
-        //   shhot_motor.write(angel);
-        //   delay(motor_delay);
-        //   shhot_motor.write(end_angel);
-        //   debugger_shoot.sPrint("target_time", target_time, "ms");
-        //   debugger_shoot.sPrint("normal -if delay time is ", delay_time_shoot, "ms");
-        //   }
-        // }     
-        if (time_resto > target_time && time_resto< 15000)
-        {
-          delay_time_shoot = time_resto - target_time;
-          if (delay_time_shoot<15000 )
-          {
-            delay(delay_time_shoot);
-          shhot_motor.write(angel);
-          delay(motor_delay);
-          shhot_motor.write(end_angel);
-          debugger_shoot.sPrint("target_time", target_time, "ms");
-          debugger_shoot.sPrint("rest -if delay time is ", delay_time_shoot, "ms");
-          debugger_shoot.sPrint("rest -if window ", window, "ms");
-          }
-          else
-          {
-            debugger_shoot.sPrint("rest time delay s big ", delay_time_shoot, "ms");
-          }
-          
-        }
-        else if (total_time > target_time)
-        {
-            delay_time_shoot=total_time-(target_time -time_resto);
-            if (delay_time_shoot<15000 )
-          {
-            delay(delay_time_shoot);
-          shhot_motor.write(angel);
-          delay(motor_delay);
-          shhot_motor.write(end_angel);
-          debugger_shoot.sPrint("target_time", target_time, "ms");
-          debugger_shoot.sPrint(" total if delay time is ", delay_time_shoot, "ms");
-          debugger_shoot.sPrint("rest -if window ", window, "ms");
-          }
-          else
-          {
-            debugger_shoot.sPrint("target_time", target_time, "ms");
-            debugger_shoot.sPrint("total time delay s big ", delay_time_shoot, "ms");
-          }
-         }
-        else
-        {
-          debugger_shoot.sPrint("target_time", target_time, "ms");
-          debugger_shoot.sPrint("there is error we couldn't shoot",0,"");
-        }
-        
+  //  if (time_resto < target_time+delta_hoder && time_resto >target_time)
+  // {
+  //   delay_time_shoot = time_resto - target_time-10;
+  //   if (delay_time_shoot<15000 )
+  //   {
+  //   delay(delay_time_shoot);
+  //   shhot_motor.write(angel);
+  //   delay(motor_delay);
+  //   shhot_motor.write(end_angel);
+  //   debugger_shoot.sPrint("target_time", target_time, "ms");
+  //   debugger_shoot.sPrint("normal -if delay time is ", delay_time_shoot, "ms");
+  //   }
+  // }
+  if (time_resto > target_time && time_resto < 15000)
+  {
+    delay_time_shoot = time_resto - target_time;
+    if (delay_time_shoot < 15000)
+    {
+      delay(delay_time_shoot);
+      shhot_motor.write(angel);
+      delay(motor_delay);
+      shhot_motor.write(end_angel);
+      debugger_shoot.sPrint("target_time", target_time, "ms");
+      debugger_shoot.sPrint("rest -if delay time is ", delay_time_shoot, "ms");
+      debugger_shoot.sPrint("rest -if window ", window, "ms");
+    }
+    else
+    {
+      debugger_shoot.sPrint("rest time delay s big ", delay_time_shoot, "ms");
+    }
+  }
+  else if (total_time > target_time)
+  {
+    delay_time_shoot = total_time - (target_time - time_resto);
+    if (delay_time_shoot < 15000)
+    {
+      delay(delay_time_shoot);
+      shhot_motor.write(angel);
+      delay(motor_delay);
+      shhot_motor.write(end_angel);
+      debugger_shoot.sPrint("target_time", target_time, "ms");
+      debugger_shoot.sPrint(" total if delay time is ", delay_time_shoot, "ms");
+      debugger_shoot.sPrint("rest -if window ", window, "ms");
+    }
+    else
+    {
+      debugger_shoot.sPrint("target_time", target_time, "ms");
+      debugger_shoot.sPrint("total time delay s big ", delay_time_shoot, "ms");
+    }
+  }
+  else
+  {
+    debugger_shoot.sPrint("target_time", target_time, "ms");
+    debugger_shoot.sPrint("there is error we couldn't shoot", 0, "");
+  }
 }
--- src/speed.cpp
+++ src/speed.cpp
@@ -2,7 +2,7 @@
 #include "Arduino.h"
 #include "Servo.h"
 #include "debug.h"
- #include "math.h"
+#include "math.h"
 debug debugger_speed;
 #define PI 3.1415926535897932384626433832795
 uint16_t NEW_DELAY_SPEED;
@@ -15,69 +15,67 @@ speed::speed()
 
 float speed::photoSpeed(int delta_pho)
 {
-return((PI/6)*1000/delta_pho);
+    return ((PI / 6) * 1000 / delta_pho);
 }
 float speed::hallSpeed(int delta_hal)
 {
-return(PI*1000/delta_hal);
+    return (PI * 1000 / delta_hal);
 }
-float speed::photoRst(int photo_seco,int delta_pho_rst,float accelo_1)
+float speed::photoRst(int photo_seco, int delta_pho_rst, float accelo_1)
 {
-    float photo_speed_intern=photoSpeed(delta_pho_rst);
-    float help_val_theta=((photo_seco)*(PI/6));
-    float result=solveTimeEquation(accelo_1,photo_speed_intern,help_val_theta);
+    float photo_speed_intern = photoSpeed(delta_pho_rst);
+    float help_val_theta = ((photo_seco) * (PI / 6));
+    float result = solveTimeEquation(accelo_1, photo_speed_intern, help_val_theta);
     return result;
 }
-float speed::hallRst(int hall_seco,int delta_hall_rst,float accelo_2)
+float speed::hallRst(int hall_seco, int delta_hall_rst, float accelo_2)
 {
-    float hall_speed_intern=hallSpeed(delta_hall_rst);
-    float help_val_theta=(hall_seco)*PI;
-    float result=solveTimeEquation(accelo_2,hall_speed_intern,help_val_theta);
+    float hall_speed_intern = hallSpeed(delta_hall_rst);
+    float help_val_theta = (hall_seco)*PI;
+    float result = solveTimeEquation(accelo_2, hall_speed_intern, help_val_theta);
     return result;
 }
-float speed::totalHallTime(int delto_hall,float accelo_3)
+float speed::totalHallTime(int delto_hall, float accelo_3)
 {
-    float hallSpeedo=hallSpeed(delto_hall);
-    return(solveTimeEquation(accelo_3,hallSpeedo,2*PI));
+    float hallSpeedo = hallSpeed(delto_hall);
+    return (solveTimeEquation(accelo_3, hallSpeedo, 2 * PI));
 }
-float speed::totalPhotoTime(int delto_photo,float accelo_4)
+float speed::totalPhotoTime(int delto_photo, float accelo_4)
 {
-    float photoSpeedo=photoSpeed(delto_photo);
-    float result=(solveTimeEquation(accelo_4,photoSpeedo,2*PI));
+    float photoSpeedo = photoSpeed(delto_photo);
+    float result = (solveTimeEquation(accelo_4, photoSpeedo, 2 * PI));
     return result;
 }
-boolean speed::secureMotion(int val1, int val2,boolean start_FLAG)
+boolean speed::secureMotion(int val1, int val2, boolean start_FLAG)
 {
-  if (millis()>5000)
-  {
-    if(start_FLAG==true)
+    if (millis() > 5000)
     {
-      Serial.println("the check is started");
-      start_FLAG=false;
+        if (start_FLAG == true)
+        {
+            Serial.println("the check is started");
+            start_FLAG = false;
+        }
+        if (abs(val1 - val2) > val2 || abs(val1 - val2) > val1)
+        {
+            while (1)
+            {
+                Serial.print(" Something is Wrong please Restart the deference =   ");
+                Serial.println(abs(val1 - val2));
+                delay(500);
+            }
+        }
     }
-    if (abs(val1-val2)> val2 || abs(val1-val2)> val1)
-  {
-      while (1)
-      {
-        Serial.print(" Something is Wrong please Restart the deference =   ");
-        Serial.println(abs(val1-val2));
-        delay(500);
-      }
-  }
-  }
-  return start_FLAG;
-  
+    return start_FLAG;
 }
 
-//******* 
+//*******
 void speed::checkStop(int time1, int time2)
-{ 
-    if (time1 == time2 )
-    { 
+{
+    if (time1 == time2)
+    {
         Serial.print(" Errror Error Errror   the check position error   ");
         Serial.println(time1);
         Serial.println(time2);
-        
     }
 }
 float speed::getThetavalues(int time_interval, int time_target_val, float angular_acceleartion_val, char xval)
@@ -85,85 +83,87 @@ float speed::getThetavalues(int time_interval, int time_target_val, float angula
     switch (xval)
     {
     case 'x':
-        return (2*PI)+(photoSpeed(time_interval)*time_target_val/1000)-(0.5*angular_acceleartion_val*((time_target_val/1000)*(time_target_val/1000)));
+        return (2 * PI) + (photoSpeed(time_interval) * time_target_val / 1000) - (0.5 * angular_acceleartion_val * ((time_target_val / 1000) * (time_target_val / 1000)));
         break;
     case 't':
-        float x_help=(2*PI)+(photoSpeed(time_interval)*time_target_val/1000)-(0.5*angular_acceleartion_val*((time_target_val/1000)*(time_target_val/1000)));
-        return x_help-(photoSpeed(time_interval)*time_target_val/1000)-(0.5*angular_acceleartion_val*((time_target_val/1000)*(time_target_val/1000)));
+        float x_help = (2 * PI) + (photoSpeed(time_interval) * time_target_val / 1000) - (0.5 * angular_acceleartion_val * ((time_target_val / 1000) * (time_target_val / 1000)));
+        return x_help - (photoSpeed(time_interval) * time_target_val / 1000) - (0.5 * angular_acceleartion_val * ((time_target_val / 1000) * (time_target_val / 1000)));
     default:
         break;
     }
 }
 
 // Calculate the time values
- float speed::solveTimeEquation(float a1, float b1,float c1)
- {
-     float c;
-     if (b1 >12 && b1<25)
-     {
-         c=-(2*PI-(c1+4*PI));
-         Serial.println("speed is bigger than 12 rad/s");
-     }
-     else if (b1<12)
-     {
-         c=-(2*PI-(c1));
-         Serial.println("speed is under 12 rad/s");
-     }
-     else if (b1 >25)
-     {
-         c=-(2*PI-(c1+6*PI));
-         Serial.println("speed is bigger than 25 rad/s");
-     }
+float speed::solveTimeEquation(float a1, float b1, float c1)
+{
+    float c;
+    if (b1 > 12 && b1 < 25)
+    {
+        c = -(2 * PI - (c1 + 4 * PI));
+        Serial.println("speed is bigger than 12 rad/s");
+    }
+    else if (b1 < 12)
+    {
+        c = -(2 * PI - (c1));
+        Serial.println("speed is under 12 rad/s");
+    }
+    else if (b1 > 25)
+    {
+        c = -(2 * PI - (c1 + 6 * PI));
+        Serial.println("speed is bigger than 25 rad/s");
+    }
     //   c=-(2*PI-(c1+4*PI));
-   
-   float a= a1/2, b=b1;
-   float  x1, x2, discriminant,real_part,imaginary_part;
-   discriminant = (b*b)- (4*a*c);
-  // debugger_speed.sPrint("a from time function ",a,"");
-   //debugger_speed.sPrint("b from time function ",b,"");
-   //debugger_speed.sPrint("c from time function ",c,"");
-  // debugger_speed.sPrint("discriminant from time function ",discriminant,"");
-                    Serial.print("a  ");
-                    Serial.println(a);
-                    Serial.print("b  ");
-                    Serial.println(b);
-                    Serial.print("c  ");
-                    Serial.println(c);
-   if (a!=0){
-    if (discriminant > 0 && a !=0) {
-        x1 = (-b + sqrt(discriminant)) / (2*a);
-        x2 = (-b - sqrt(discriminant)) / (2*a);
+
+    float a = a1 / 2, b = b1;
+    float x1, x2, discriminant, real_part, imaginary_part;
+    discriminant = (b * b) - (4 * a * c);
+    // debugger_speed.sPrint("a from time function ",a,"");
+    //debugger_speed.sPrint("b from time function ",b,"");
+    //debugger_speed.sPrint("c from time function ",c,"");
+    // debugger_speed.sPrint("discriminant from time function ",discriminant,"");
+    Serial.print("a  ");
+    Serial.println(a);
+    Serial.print("b  ");
+    Serial.println(b);
+    Serial.print("c  ");
+    Serial.println(c);
+    if (a != 0)
+    {
+        if (discriminant > 0 && a != 0)
+        {
+            x1 = (-b + sqrt(discriminant)) / (2 * a);
+            x2 = (-b - sqrt(discriminant)) / (2 * a);
             // return just the small positive values
-            if (x1 >0)
+            if (x1 > 0)
             {
-                if (x1 >x2 && x2 <15 && x2 >0)
+                if (x1 > x2 && x2 < 15 && x2 > 0)
                 {
                     //debugger_speed.sPrint("the smallest time soluation is x2  ",x2,"sec");
                     Serial.print(" the time soluation x2  ");
                     Serial.println(x2);
-                    return 1000*x2;
+                    return 1000 * x2;
                 }
-                else if (x2 > x1 && x1<15)
+                else if (x2 > x1 && x1 < 15)
                 {
                     //debugger_speed.sPrint("the smallest time soluation is x1  ",x1,"sec");
                     Serial.print(" the time soluation x1  ");
                     Serial.println(x1);
-                    return 1000*x1;
+                    return 1000 * x1;
                 }
 
-                else if (x2 >0 && x2 < 15)
+                else if (x2 > 0 && x2 < 15)
                 {
-                     //debugger_speed.sPrint("the smallest time soluation is x2  ",x2,"sec");
+                    //debugger_speed.sPrint("the smallest time soluation is x2  ",x2,"sec");
                     Serial.print(" the time soluation x2  ");
                     Serial.println(x2);
-                    return 1000*x2;
+                    return 1000 * x2;
                 }
-                else if (x1 >x2 && x1 < 15)
+                else if (x1 > x2 && x1 < 15)
                 {
-                     //debugger_speed.sPrint("the smallest time soluation is x1  ",x1,"sec");
+                    //debugger_speed.sPrint("the smallest time soluation is x1  ",x1,"sec");
                     Serial.print(" the time soluation x1  ");
                     Serial.println(x1);
-                    return 1000*x1;
+                    return 1000 * x1;
                 }
                 else
                 {
@@ -176,12 +176,12 @@ float speed::getThetavalues(int time_interval, int time_target_val, float angula
             }
             else
             {
-                if (x2 >0 && x2 <15)
+                if (x2 > 0 && x2 < 15)
                 {
                     //debugger_speed.sPrint("the smallest time soluation is x2  ",x2,"sec");
                     Serial.print(" the time soluation x2  ");
                     Serial.println(x2);
-                    return 1000*x2;
+                    return 1000 * x2;
                 }
                 else
                 {
@@ -192,85 +192,84 @@ float speed::getThetavalues(int time_interval, int time_target_val, float angula
                     Serial.println(x2);
                 }
             }
-            
+        }
+
+        else if (discriminant == 0 && a != 0)
+        {
+            x1 = (-b + sqrt(discriminant)) / (2 * a);
+            // debugger_speed.sPrint("there is only one soluation  ",x1,"sec");
+            Serial.print(" the time soluation x1 doupple  ");
+            Serial.println(x1);
+            return 1000 * x1;
+        }
+        else
+        {
+
+            real_part = -b / (2 * a);
+            imaginary_part = sqrt(-discriminant) / (2 * a);
+            x1 = sqrt((real_part * real_part) + (imaginary_part * imaginary_part));
+            //debugger_speed.sPrint("the soluation is complex and abs value for the real and imaginary parts are  ",x1,"sec");
+            if (b1 > 12 && b1 < 25)
+            {
+                c = 4 * PI - c1;
+                Serial.print(" 4*PI-c1  cx  ");
+                Serial.println(c1);
+            }
+            else if (b1 < 12)
+            {
+                c = 2 * PI - c1;
+                Serial.print(" c=2*PI-c1 cx  ");
+                Serial.println(c1);
+            }
+            else if (b1 > 25)
+            {
+                c = 6 * PI - c1;
+                Serial.print(" c=6*PI-c1  cx  ");
+                Serial.println(c1);
+            }
+            x2 = c / b;
+            //debugger_speed.sPrint("the alternative soluation -c/b  ",x2,"sec");
+            // debugger_speed.sPrint("c1  ",c1,"rad");
+            // debugger_speed.sPrint("b  ",b,"rad/s");
+            Serial.print(" the alternative soluation c/b   ");
+            Serial.println(x2);
+            Serial.print(" c  ");
+            Serial.println(c);
+            Serial.print(" b  ");
+            Serial.println(b);
+            return 1000 * x2;
+        }
     }
-    
-    else if (discriminant == 0 && a !=0)
+    else
     {
-        x1 = (-b + sqrt(discriminant)) / (2*a);
-       // debugger_speed.sPrint("there is only one soluation  ",x1,"sec");
-        Serial.print(" the time soluation x1 doupple  ");
+
+        //check speed and choose the best theta value to avoid smal time and complex soluations
+        if (b1 > 12 && b1 < 25)
+        {
+            c = 4 * PI - c1;
+            Serial.print(" 4*PI-c1  ");
+            Serial.println(c1);
+        }
+        else if (b1 < 12)
+        {
+            c = 2 * PI - c1;
+            Serial.print(" c=2*PI-c1 ");
+            Serial.println(c1);
+        }
+        else if (b1 > 25)
+        {
+            c = 6 * PI - c1;
+            Serial.print(" c=6*PI-c1  ");
+            Serial.println(c1);
+        }
+        x1 = c / b;
+        // debugger_speed.sPrint("the acceleration is 0 and there is only one soluation x1  ",x1,"sec");
+        Serial.print(" the time soluation c/b  ");
         Serial.println(x1);
-        return 1000*x1;
+        Serial.print(" c  ");
+        Serial.println(c);
+        Serial.print(" b  ");
+        Serial.println(b);
+        return 1000 * x1;
     }
-    else {
-        
-        real_part=-b/(2*a);
-        imaginary_part=sqrt(-discriminant)/(2*a);
-        x1=sqrt((real_part*real_part)+(imaginary_part*imaginary_part));
-        //debugger_speed.sPrint("the soluation is complex and abs value for the real and imaginary parts are  ",x1,"sec");
-    if (b1 >12 && b1<25)
-     {
-       c=4*PI-c1;
-       Serial.print(" 4*PI-c1  cx  ");
-       Serial.println(c1);
-     }
-     else if (b1<12)
-     {
-       c=2*PI-c1;
-       Serial.print(" c=2*PI-c1 cx  ");
-       Serial.println(c1);
-     }
-     else if (b1 >25)
-     {
-       c=6*PI-c1;
-       Serial.print(" c=6*PI-c1  cx  ");
-       Serial.println(c1);
-     }
-        x2  = c/b;
-        //debugger_speed.sPrint("the alternative soluation -c/b  ",x2,"sec");
-       // debugger_speed.sPrint("c1  ",c1,"rad");
-       // debugger_speed.sPrint("b  ",b,"rad/s");
-       Serial.print(" the alternative soluation c/b   ");
-       Serial.println(x2);
-       Serial.print(" c  ");
-       Serial.println(c);
-       Serial.print(" b  ");
-       Serial.println(b);
-        return 1000*x2; 
-    }
-
-   }
-   else
-   {
-
-            //check speed and choose the best theta value to avoid smal time and complex soluations 
-            if (b1 >12 && b1<25)
-            {
-                c=4*PI-c1;
-                Serial.print(" 4*PI-c1  ");
-                Serial.println(c1);
-            }
-            else if (b1<12)
-            {
-                c=2*PI-c1;
-                Serial.print(" c=2*PI-c1 ");
-                Serial.println(c1);
-            }
-            else if (b1 >25)
-            {
-                c=6*PI-c1;
-                Serial.print(" c=6*PI-c1  ");
-                Serial.println(c1);
-            }
-        x1 = c/b;
-       // debugger_speed.sPrint("the acceleration is 0 and there is only one soluation x1  ",x1,"sec");
-       Serial.print(" the time soluation c/b  ");
-       Serial.println(x1);
-       Serial.print(" c  ");
-       Serial.println(c);
-       Serial.print(" b  ");
-       Serial.println(b);
-       return 1000*x1;
-   }
 }

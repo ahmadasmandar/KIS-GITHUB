e3ddc5c0385dd50ed495b3f6b4ef942ca5f8558a
Ahmad Asmandar <ahmedasmndr2@gmail.com>
2019-07-08 01:26:43 +0200

    pyplot from python

---
+++ Values/SensorValues/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+from matplotlib import pyplot as plt
+p = open('photovalues-clean.txt', 'r+')
+h = open('hallvalues-clean.txt', 'r+')
+content_photo=p.readlines()
+x=[]
+i=0
+for val in content_photo:
+    x.append(int(val.rstrip("\n")))
+    i+=1
+y=(i+1)*30
+time_1=np.arange(30,y,30)
+print(len(x))
+print(len(time_1))
+plt.plot(time_1,x)
+plt.ylim(0,750)
+plt.xlim(0,5000)
+plt.grid(True)
+plt.show()
+
+

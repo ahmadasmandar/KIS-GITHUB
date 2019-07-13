376a370e84fe98decb9b4227df41ab51115ee876
Ahmad Asmandar <ahmedasmndr2@gmail.com>
2019-07-14 01:14:41 +0200

    order data lists using python

--- Data-Analyse/Durchfälle/main.py
+++ Data-Analyse/Durchfälle/main.py
@@ -1,25 +1,27 @@
 
 from matplotlib import pyplot as plt
-f = open('photo fast 6', 'r+')
+f = open('durchfall1.txt', 'r+')
 content=f.readlines()
 target_time=[]
 angular_speed=[]
 deference=[]
-with open('data/target_time_addition_value_photo_1.txt','w') as outfile2:
+with open('data/n1.txt','w') as outfile2:
     for x in content:
         # outfile2.write(x)
         if "target_time"in x:
             # x=x.replace("time delta_photo","").lstrip()
-            # outfile2.write(x)
+            outfile2.write(x)
             target_time.append(float(x.replace("target_time","").replace("ms","").lstrip().rstrip("\n")))
         if "w rad/s" in x:
-            # outfile2.write(x)
+            outfile2.write(x)
             angular_speed.append(float(x.replace("w rad/s","").lstrip().rstrip("\n")))
-target_time.sort()
+        if "angular_speed" in x:
+            angular_speed.append(float(x.replace("angular_speed ", "").replace("rad/s","").lstrip().rstrip("\n")))
+red=0
 angular_speed.sort(reverse=True)
+target_time.sort()
+    
 print(target_time)
-# print(len(target_time))
-# print(len(angular_speed))
 print(angular_speed)
 i=0
 for val in target_time:
@@ -29,6 +31,6 @@ for val in target_time:
 print(deference)
 # plt.plot(target_time,angular_speed, color='r',label="speed")
 # plt.plot(target_time,deference,label="defrence")
-plt.plot(angular_speed,deference)
-# plt.legend()
+plt.plot(angular_speed,deference,label="speed/defrence")
+plt.legend()
 plt.show()

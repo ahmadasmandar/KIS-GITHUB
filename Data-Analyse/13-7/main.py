371ca9f560732ab3f67653d27a656bfa9e9b238e
ahmadasmndr <ahmedasmndr2@gmail.com>
2019-07-13 21:11:52 +0200

    add values 1307

---
+++ Data-Analyse/13-7/main.py
@@ -0,0 +1,33 @@
+
+from matplotlib import pyplot as plt
+f = open('photo fast 6', 'r+')
+content=f.readlines()
+target_time=[]
+angular_speed=[]
+deference=[]
+with open('data/target_time_addition_value_photo_1.txt','w') as outfile2:
+    for x in content:
+        # outfile2.write(x)
+        if "target_time"in x:
+            # x=x.replace("time delta_photo","").lstrip()
+            # outfile2.write(x)
+            target_time.append(float(x.replace("target_time","").replace("ms","").lstrip().rstrip("\n")))
+        if "w rad/s" in x:
+            # outfile2.write(x)
+            angular_speed.append(float(x.replace("w rad/s","").lstrip().rstrip("\n")))
+
+print(target_time)
+print(len(target_time))
+print(len(angular_speed))
+print(angular_speed)
+i=0
+for val in target_time:
+    relation=(val-391)
+    deference.append(round(relation,2))
+    i+=1
+print(deference)
+# plt.plot(target_time,angular_speed, color='r',label="speed")
+# plt.plot(target_time,deference,label="defrence")
+plt.plot(angular_speed,deference)
+# plt.legend()
+plt.show()

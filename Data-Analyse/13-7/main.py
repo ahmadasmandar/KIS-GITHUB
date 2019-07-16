c40d8922bfde8f6d7b2f3dbe8eb57558f0dd31dc
Ahmad Asmandar <ahmedasmndr2@gmail.com>
2019-07-16 09:38:54 +0200

    python alone do it

--- Data-Analyse/13-7/main.py
+++ Data-Analyse/13-7/main.py
@@ -1,20 +1,22 @@
 
+#https://plot.ly/matplotlib/polynomial-fits/
 from matplotlib import pyplot as plt
+import numpy as np
 f = open('100 % photo', 'r+')
 content=f.readlines()
 target_time=[]
 angular_speed=[]
 deference=[]
-with open('data/target_time_addition_value_photo_1.txt','w') as outfile2:
-    for x in content:
+# with open('data/target_time_addition_value_photo_1.txt','w') as outfile2:
+for x in content:
+    # outfile2.write(x)
+    if "target_time"in x:
+        # x=x.replace("time delta_photo","").lstrip()
         # outfile2.write(x)
-        if "target_time"in x:
-            # x=x.replace("time delta_photo","").lstrip()
-            # outfile2.write(x)
-            target_time.append(float(x.replace("target_time","").replace("ms","").lstrip().rstrip("\n")))
-        if "w rad/s" in x:
-            # outfile2.write(x)
-            angular_speed.append(float(x.replace("w rad/s","").lstrip().rstrip("\n")))
+        target_time.append(float(x.replace("target_time","").replace("ms","").lstrip().rstrip("\n")))
+    if "w rad/s" in x:
+        # outfile2.write(x)
+        angular_speed.append(float(x.replace("w rad/s","").lstrip().rstrip("\n")))
 target_time.sort()
 angular_speed.sort(reverse=True)
 print(target_time)
@@ -29,6 +31,13 @@ for val in target_time:
 print(deference)
 # plt.plot(target_time,angular_speed, color='r',label="speed")
 # plt.plot(target_time,deference,label="defrence")
-plt.plot(angular_speed,deference)
-# plt.legend()
+# calculate polynomial
+z = np.polyfit(angular_speed,deference,1)
+f = np.poly1d(z)
+new_range=np.linspace(0,30,50)
+y_new=f(new_range)
+print(f)
+plt.plot(angular_speed,deference,color='red',label="data")
+plt.plot(new_range,y_new,color='g',label=f)
+plt.legend()
 plt.show()
